a5fbdecab74ba02659ae47229595e98ccbddc4d6
fred <daktarispacetime@gmail.com>
2023-04-02 20:14:42 +0300

    added secret for deployment

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+
+  get 'movies', to: 'movies#index'
+  get 'actors', to: 'actors#index'
+  get 'users', to: 'users#index'
+  get 'reviews', to: 'reviews#index'
+
+  
 end
